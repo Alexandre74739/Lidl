7345e9859e693ac96b111a83ff28be25d8849ea1
koffiwilly <willy.koffi@outlook.fr>
2026-04-15 14:51:33 +0200

    cc first commit willy

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c84425d6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.va0slg06r4k"
+    "revision": "0.fs62t2oe71o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
